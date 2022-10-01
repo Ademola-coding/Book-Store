3b5209192c87bcc4ecc803be7c3e6148b672603c
ademola <ademolaowoeye07@gmail.com>
2022-10-01 21:30:18 +0100

    Fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Store from './redux/ConfigureStore'
+import Store from './redux/ConfigureStore';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
