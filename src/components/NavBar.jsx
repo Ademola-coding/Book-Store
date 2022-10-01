a1e566a107411890827c8ade611844ae95c6a5fd
ademola <ademolaowoeye07@gmail.com>
2022-10-01 20:40:46 +0100

    MADE STYLING

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-  <div>
-    <div>
-      <h1>Bookstore</h1>
-    </div>
-    <div >
+  <div className='naving'>
+      <h1 className='logo'>Bookstore</h1>
+    <div className='links'>
       <Link to="/Books" className='link' >Books</Link>
       <Link to="/Categories" className='link'>Categories</Link>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './components/App/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
